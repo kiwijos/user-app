2fe3b5dcad6b2e38c5dcef55773afd27ecb9c9d3
kiwijos <allq22@student.bth.se>
2024-01-09 23:23:55 +0100

    Expose style props and add more pathname mappings

--- src/lib/components/Breadcrumb.svelte
+++ src/lib/components/Breadcrumb.svelte
@@ -1,15 +1,25 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 
+	export let text: string = 'capitalize text-surface-700 dark:text-surface-300';
+	export let padding: string = 'px-4 py-2';
+	export let background: string = 'bg-surface-50 dark:bg-surface-900';
+	export let border: string = 'dark:border-b dark:border-surface-800';
+	export let shadow: string = 'drop-shadow-sm dark:shadow-none';
+
 	const names: { [key: string]: string } = {
 		account: 'Profil',
 		transactions: 'Betalningar',
 		payment: 'Kortuppgifter',
 		settings: 'Inställningar',
-		trips: 'Resehistorik'
+		trips: 'Resor',
+		search: 'Sök',
+		rent: 'Hyr',
+		STHLM: 'Stockholm',
+		GBG: 'Göteborg',
+		KRLST: 'Karlstad'
 	};
 
-	// create breadcrumbs for all pages after /admin
 	$: myBreadcrumbs = $page.url.pathname
 		.split('/')
 		.slice(2)
@@ -22,9 +32,10 @@
 					.join('/')
 			};
 		});
+	$: size = myBreadcrumbs.length;
 </script>
 
-<ol class="breadcrumb capitalize" data-sveltekit-preload-data="false">
+<ol class="breadcrumb {size === 0 ? 'hidden' : ''} {text} {padding} {background} {border} {shadow}">
 	{#each myBreadcrumbs as crumb, i}
 		{#if i < myBreadcrumbs.length - 1}
 			<li class="crumb"><a class="anchor" href={crumb.link}>{crumb.label}</a></li>
