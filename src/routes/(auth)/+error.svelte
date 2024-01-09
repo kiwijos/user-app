d4c60b3759a810a6b76130026f93408cd0d67c83
kiwijos <allq22@student.bth.se>
2024-01-09 23:27:42 +0100

    Rework for mobile with single button to login page

--- src/routes/(auth)/+error.svelte
+++ src/routes/(auth)/+error.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import Fa from 'svelte-fa';
+	import { faArrowRotateBack } from '@fortawesome/free-solid-svg-icons';
 </script>
 
-<h2 class="h2 font-thin text-surface-700 dark:text-surface-300">
+<h1 class="text-center text-4xl font-extrabold">Nåt gick fel</h1>
+<h2 class="text-center h3 font-medium text-surface-500">
 	{$page.error?.message}
 </h2>
-<p class="font-medium text-surface-700 dark:text-surface-300">
-	Har du ett konto? Försök igen. <a href="/login" class="anchor">Logga in</a>
-</p>
-
-<p class="font-medium text-surface-700 dark:text-surface-300">
-	Eller har du bara hamnat fel? <a href="/" class="anchor">Startsidan</a>
-</p>
+<a
+	href="/"
+	title="Tillbaka till inloggning"
+	class="btn rounded-container-token w-full md:max-w-72 p-4 bg-surface-900 border border-surface-800 flex items-center justify-center space-x-2 text-surface-700 dark:text-surface-300"
+	><span><Fa icon={faArrowRotateBack} /></span><span>Tillbaka till inloggning</span></a
+>
