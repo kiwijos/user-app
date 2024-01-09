8611f24cdd65914aac9fd4cb23b011864658e5a9
kiwijos <allq22@student.bth.se>
2024-01-10 00:16:24 +0100

    Remove unused library

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,6 @@
 	import { autoModeWatcher, LightSwitch } from '@skeletonlabs/skeleton';
 	import { Toast } from '@skeletonlabs/skeleton';
 	import { initializeStores } from '@skeletonlabs/skeleton';
-	import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
-	import { storePopup } from '@skeletonlabs/skeleton';
-	storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
 
 	initializeStores();
 </script>
