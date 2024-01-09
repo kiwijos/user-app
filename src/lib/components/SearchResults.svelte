f95415356835e9721df3b5cb42dd51e75d5d295d
kiwijos <allq22@student.bth.se>
2024-01-10 00:12:22 +0100

    Create a list of search results where matched substrings are highlighted
    This component can be used with the SearchInput component.

---
+++ src/lib/components/SearchResults.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+	import type { SearchResult } from '$lib/types/SearchResult';
+
+	export let results: SearchResult[] = [];
+	export let highlightedSubstring = '';
+
+	/**
+	 * Arbitrary HTML (as string) to display when there are no matches
+	 * If this is set, noMatchesText will be ignored
+	 * default: undefined
+	 * example: '<p class="text-center text-surface-500">No matches</p>'
+	 * @type {string}
+	 */
+	export let noMatchesHTML: string | undefined = undefined;
+
+	/**
+	 * The text to display when there are no matches
+	 * default: 'Inga träffar'
+	 * @type {string}
+	 */
+	export let noMatchesText: string = 'Inga träffar';
+
+	/**
+	 * Ultimately, this is the content that will be displayed when there are no matches
+	 * @type {string}
+	 */
+	let noMatchesContent: string =
+		noMatchesHTML ?? `<p class="text-center text-surface-500">${noMatchesText}</p>`;
+
+	/** The background and text style of the highlighted substring
+	 * default: 'variant-soft-primary font-bold'
+	 * @type {string}
+	 */
+	export let highlight: string = 'variant-soft-primary font-bold';
+
+	function highlightSubstring(text: string, substring: string) {
+		if (!substring) return text;
+		const regex = new RegExp(`(${substring})`, 'gi');
+		return text
+			.split(regex)
+			.map((part, index) => (index % 2 === 0 ? part : `<span class="${highlight}">${part}</span>`))
+			.join('');
+	}
+</script>
+
+{#if results.length > 0}
+	<div>
+		{#each results as result}
+			<button
+				class="w-full flex flex-col px-4 py-2 border-b dark:border-surface-800 last:border-none text-surface-700 dark:text-surface-300 hover:bg-surface-200 hover:dark:bg-surface-800 hover:dark:text-white"
+				on:click={() => result.callback(result.value)}
+				><span class="text-xl">{@html highlightSubstring(result.name, highlightedSubstring)}</span>
+				<span class="text-base text-surface-500 dark:text-surface-500"
+					>{@html highlightSubstring(result.alias.join(' '), highlightedSubstring)}</span
+				></button
+			>
+		{/each}
+	</div>
+{:else}
+	{@html noMatchesContent}
+{/if}
