4e6af211dcf5b004c826577abe9503f312b33394
kiwijos <allq22@student.bth.se>
2024-01-09 23:50:28 +0100

    Rework for mobile

--- src/routes/(main)/me/account/+page.svelte
+++ src/routes/(main)/me/account/+page.svelte
@@ -3,34 +3,35 @@
 	import { Avatar } from '@skeletonlabs/skeleton';
 	import type { PageData } from './$types';
 	import Fa from 'svelte-fa';
-	import { faXmark } from '@fortawesome/free-solid-svg-icons';
 	import UserNav from '$lib/components/UserNav.svelte';
 	import { cardOptions } from '$lib/utils/cardOptions';
 
 	export let data: PageData;
 </script>
 
-<div class="space-y-8">
-	<div class="flex space-x-4 items-start">
-		<Avatar background="bg-gray-200 dark:bg-surface-600" initials={$page.data.user.email[0]} />
+<div class="space-y-4">
+	<div class="flex space-x-4 items-center">
+		<Avatar background="bg-gray-200 dark:bg-surface-800" initials={$page.data.user.email[0]} />
 		<div>
-			<p class="text-2xl capitalize">{$page.data.user.email.split('@')[0]}</p>
-			<p class="text-gray-500 dark:text-gray-400">{$page.data.user.email}</p>
+			<p class="text-2xl text-surface-700 dark:text-surface-300 capitalize">
+				{$page.data.user.email.split('@')[0]}
+			</p>
+			<p class="text-surface-500">{$page.data.user.email}</p>
 		</div>
 	</div>
 	{#if data?.card !== undefined}
 		<div class="flex flex-col-reverse xs:flex-row gap-4">
 			<a
 				href="/me/account/payment"
-				class="p-2 card card-hover h-28 w-full sm:w-52 border-2 border-primary-500 dark:border-tertiary-700 grid grid-cols-3 grid-rows-3 rounded-container-token bg-gradient-to-br from-primary-500 via-tertiary-300 to-secondary-400 dark:from-tertiary-800 dark:via-tertiary-900 dark:to-primary-800"
+				class="p-2 card card-hover h-44 w-full sm:w-52 border-2 border-primary-500 dark:border-tertiary-700 grid grid-cols-3 grid-rows-3 rounded-container-token bg-gradient-to-br from-primary-500 via-tertiary-300 to-secondary-400 dark:from-tertiary-800 dark:via-tertiary-900 dark:to-primary-800"
 			>
 				<div
-					class="text-2xl col-start-3 row-start-1 justify-self-end text-white dark:text-primary-200"
+					class="text-3xl col-start-3 row-start-1 justify-self-end text-white dark:text-primary-200"
 				>
 					<Fa icon={cardOptions[data.card.card_type - 1].icon} />
 				</div>
 				<p
-					class="text-white dark:text-primary-200 text-center text-sm text-shadow text-shadow-sm shadow-primary-900 col-span-3 row-start-3"
+					class="text-white dark:text-primary-200 text-center text-base text-shadow text-shadow-sm shadow-primary-900 col-span-3 row-start-3"
 				>
 					{data.card.card_nr
 						? `${data.card.card_nr.substring(0, 4)} XXXX XXXX XXXX`
