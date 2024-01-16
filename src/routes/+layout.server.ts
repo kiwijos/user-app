4e1834bce0355c7a7bd175f11f4c927ccdae205f
kiwijos <allq22@student.bth.se>
2024-01-16 23:12:44 +0100

    Fetch cities and zones at the root
    The cities and zones data is essentially static. By fetching the data at the root level, we can avoid fetching it multiple times.

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,8 +1,37 @@
 import type { LayoutServerLoad } from './$types';
+import { PUBLIC_REST_API_URL } from '$env/static/public';
+import type { City } from '$lib/types/City';
+import type { Zone } from '$lib/types/Zone';
 
 // pass `locals.user` to the `page` store for use inside client-side code
-export const load: LayoutServerLoad = async ({ locals }) => {
+export const load: LayoutServerLoad = async ({ fetch, locals }) => {
+	const cities = await fetch(`${PUBLIC_REST_API_URL}/cities`).then((r) => r.json());
+
+	const cityFeatures = cities.map((city: City) => {
+		return {
+			type: 'Feature',
+			geometry: city.geometry,
+			properties: {
+				...city
+			}
+		};
+	});
+
+	const zones = await fetch(`${PUBLIC_REST_API_URL}/zones`).then((r) => r.json());
+
+	const zonesFeatures = zones.map((zone: Zone) => {
+		return {
+			type: 'Feature',
+			geometry: zone.geometry,
+			properties: {
+				...zone
+			}
+		};
+	});
+
 	return {
-		user: locals?.user ?? null
+		user: locals?.user ?? null,
+		zones: zonesFeatures,
+		cities: cityFeatures
 	};
 };
