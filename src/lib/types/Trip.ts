fe6e2d1da0a622ff09ace399152bccbcc3785276
kiwijos <allq22@student.bth.se>
2024-01-16 22:59:47 +0100

    Make fields optional as they very well might be missing

--- src/lib/types/Trip.ts
+++ src/lib/types/Trip.ts
@@ -3,11 +3,11 @@ export interface Trip {
 	user_id: number;
 	bike_id: number;
 	start_time: string;
-	end_time: string;
-	start_cost: number;
-	park_cost: number;
-	var_cost: number;
-	total_cost: number;
+	end_time?: string;
+	start_cost?: number;
+	park_cost?: number;
+	var_cost?: number;
+	total_cost?: number;
 	start_pos: number[];
-	end_pos: number[];
+	end_pos?: number[];
 }
