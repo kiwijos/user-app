0129626770b71f01e656f1749f0969aedc078e73
kiwijos <allq22@student.bth.se>
2024-01-16 23:38:03 +0100

    Major rework for mobile
    Display transactions as clickable cards categorized by date. Clicking on a card will show a detailed view of the transaction in a second tab.

--- src/routes/(main)/me/account/transactions/+page.svelte
+++ src/routes/(main)/me/account/transactions/+page.svelte
@@ -1,19 +1,69 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-	import { Table } from '@skeletonlabs/skeleton';
-	import { tableMapperValues } from '@skeletonlabs/skeleton';
-	import type { TableSource } from '@skeletonlabs/skeleton';
-	import { Paginator } from '@skeletonlabs/skeleton';
-	import type { PaginationSettings } from '@skeletonlabs/skeleton';
+	import Fa from 'svelte-fa';
+	import { faAngleRight, faXmark } from '@fortawesome/free-solid-svg-icons';
 	import { formatDateReadable } from '$lib/services/dateFormatter';
 	import type { Transaction } from '$lib/types/Transaction';
 	import type { TransactionFormatted } from '$lib/types/TransactionFormatted';
+	import TabSwitcher from '$lib/components/TabSwitcher.svelte';
+	import TabButton from '$lib/components/TabButton.svelte';
+	import VerticalTable from '$lib/components/VerticalTable.svelte';
 
-	function onRowSelect(event: CustomEvent): void {
-		console.log(event.detail);
+	let currentTab = 0;
+
+	let selectedTransaction: TransactionFormatted | undefined;
+	let selectedRows: (string | number)[][] = [];
+	let headers: string[] = [];
+
+	$: if (selectedTransaction) {
+		selectedRows = [
+			[
+				selectedTransaction.id,
+				selectedTransaction.date_formatted,
+				selectedTransaction.ref,
+				selectedTransaction.amount_formatted
+			]
+		];
+		headers = ['Betalnr', 'Datum', 'Referens', 'Belopp'];
+	}
+
+	function handleTabButtonClick(
+		event: CustomEvent<{ index: number }>,
+		TransactionId: number | undefined
+	) {
+		currentTab = event.detail.index;
+
+		if (currentTab === 1 && TransactionId !== undefined) {
+			selectedTransaction = formattedTransactions.find(
+				(Transaction) => Transaction.id === TransactionId
+			);
+		}
+
+		if (currentTab === 0) {
+			selectedTransaction = undefined;
+		}
 	}
 
 	export let data: PageData;
+
+	function categorizeTransactionsByMonthAndYear(transactions: TransactionFormatted[]) {
+		const categorizedTransactions: { [key: string]: TransactionFormatted[] } = {};
+
+		transactions.forEach((transaction) => {
+			const startTime = new Date(transaction.date);
+			const month = startTime.toLocaleString('default', { month: 'long' });
+			const year = startTime.getFullYear();
+			const category = `${month} ${year}`;
+
+			if (!categorizedTransactions[category]) {
+				categorizedTransactions[category] = [];
+			}
+
+			categorizedTransactions[category].push(transaction);
+		});
+
+		return categorizedTransactions;
+	}
 
 	const formattedTransactions: TransactionFormatted[] = data.transactions.map(
 		(transaction: Transaction) => {
@@ -28,78 +78,55 @@
 		}
 	);
 
-	$: size = formattedTransactions.length;
-
-	// Same goes for the pagination settings
-	let paginationSettings: PaginationSettings;
-	$: paginationSettings = {
-		page: 0,
-		limit: 5,
-		size: size,
-		amounts: [1, 2, 5, 10]
-	};
-
-	// Slice the data to fit the pagination
-	$: sourceBodySliced = formattedTransactions.slice(
-		paginationSettings.page * paginationSettings.limit,
-		paginationSettings.page * paginationSettings.limit + paginationSettings.limit
-	);
-
-	// These are visible in the UI
-	$: sourceBodySlicedMapped = tableMapperValues(sourceBodySliced, [
-		'date_formatted',
-		'ref',
-		'amount_formatted'
-	]);
-
-	// These are not visible in the UI at first, but are returned when a row is selected
-	$: sourceBodyMetaSlicedMapped = tableMapperValues(sourceBodySliced, [
-		'id',
-		'user_id',
-		'ref',
-		'amount'
-	]);
-
-	let sourceHeaders: string[] = ['Datum', 'Ref', 'Pris']; // Table headers
-
-	$: totalCost = sourceBodySliced.reduce(
-		(acc: number, transaction: TransactionFormatted) => acc + transaction.amount,
-		0
-	);
-	$: numberOfTransactions = sourceBodySliced.length;
-
-	let table: TableSource;
-	$: table = {
-		head: sourceHeaders,
-		body: sourceBodySlicedMapped,
-		meta: sourceBodyMetaSlicedMapped,
-		foot: [
-			`<code class="code">${numberOfTransactions} resor</code>`,
-			'',
-			`<code class="code">${totalCost} kr</code>`
-		]
-	};
+	const categorizedTransactions = categorizeTransactionsByMonthAndYear(formattedTransactions);
 </script>
 
-<div class="space-y-4">
-	<Paginator
-		bind:settings={paginationSettings}
-		showFirstLastButtons={false}
-		showPreviousNextButtons={true}
-		amountText="Resor"
-		separatorText="av"
-		select="text-xs sm:text-sm dark:bg-surface-800 rounded-lg dark:border-surface-800"
-		controlVariant="text-xs sm:text-sm dark:bg-surface-800"
-		class="!flex-row gap-2 !space-x-0 !space-y-0 [&>label]:w-fit"
-	/>
-	<div class="overflow-x-scroll">
-		<Table
-			source={table}
-			interactive={true}
-			on:selected={onRowSelect}
-			text="text-xs"
-			regionHeadCell="uppercase"
-			regionFootCell="lowercase"
-		/>
+<TabSwitcher {currentTab}>
+	<div class="py-8 space-y-2" slot="tab1">
+		{#each Object.entries(categorizedTransactions) as [dateCategory, transactions]}
+			<div class="text-surface-500">
+				<div class="border-b border-primary-400 dark:border-primary-900 pl-4">
+					<h1 class="text-lg capitalize text-primary-400 dark:text-primary-900">
+						{dateCategory}
+					</h1>
+				</div>
+				<div class="divide-y divide-surface-300 dark:divide-surface-800">
+					{#each transactions as transaction}
+						<TabButton
+							index={1}
+							width="w-full"
+							classes="flex pl-4 py-2"
+							on:tabButtonClick={(e) => handleTabButtonClick(e, transaction.id)}
+						>
+							<div class="grow">
+								<div class="text-lg text-surface-700 dark:text-surface-300 flex justify-between">
+									<p>{transaction.amount_formatted}</p>
+									<p>Ref#{transaction.ref}</p>
+								</div>
+								<div class="text-surface-500">
+									<p>{transaction.date_formatted}</p>
+								</div>
+								<div class="text-surface-500 flex justify-between">
+									<p>Privat</p>
+									<p>Betalnr {transaction.id}</p>
+								</div>
+							</div>
+							<div class="h-full my-auto pl-2">
+								<span class="block text-surface-500 p-4"><Fa icon={faAngleRight} /></span>
+							</div>
+						</TabButton>
+					{/each}
+				</div>
+			</div>
+		{/each}
 	</div>
-</div>
+	<div class="space-y-4" slot="tab2">
+		<VerticalTable rows={selectedRows} {headers} />
+		<TabButton
+			index={0}
+			classes="btn variant-soft-surface text-lg ml-2"
+			on:tabButtonClick={(e) => handleTabButtonClick(e, selectedTransaction?.id)}
+			><span><Fa icon={faXmark} /></span><span>Visa alla</span></TabButton
+		>
+	</div>
+</TabSwitcher>
