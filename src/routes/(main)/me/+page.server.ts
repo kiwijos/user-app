6f9333fadd8c705003f4261cb6da414116796014
kiwijos <allq22@student.bth.se>
2024-01-17 14:15:29 +0100

    Filter out bikes that cannot be rented

--- src/routes/(main)/me/+page.server.ts
+++ src/routes/(main)/me/+page.server.ts
@@ -3,9 +3,13 @@ import { PUBLIC_REST_API_URL } from '$env/static/public';
 import type { Bike } from '$lib/types/Bike';
 
 export const load: PageServerLoad = async ({ fetch }) => {
-	const bikes = await fetch(`${PUBLIC_REST_API_URL}/bikes`).then((r) => r.json());
+	const bikes = (await fetch(`${PUBLIC_REST_API_URL}/bikes`)
+		.then((r) => r.json())
+		.catch(() => [])) as Bike[];
 
-	const bikeFeatures = bikes.map((bike: Bike) => {
+	const availableBikes = bikes.filter((bike: Bike) => bike.status_id !== 2 && bike.status_id !== 5);
+
+	const bikeFeatures = availableBikes.map((bike: Bike) => {
 		return {
 			type: 'Feature',
 			geometry: {
