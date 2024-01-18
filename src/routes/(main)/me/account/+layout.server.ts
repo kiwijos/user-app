8d58c182c348cf12b5cff8fcdfa8dcbb6fdc7675
kiwijos <allq22@student.bth.se>
2024-01-19 00:05:23 +0100

    Move fetch for balance to separate load function
    Separating the fetch requests makes it possible to  invalidate them separately.

--- src/routes/(main)/me/account/+layout.server.ts
+++ src/routes/(main)/me/account/+layout.server.ts
@@ -3,10 +3,10 @@ import { PUBLIC_REST_API_URL } from '$env/static/public';
 
 // pass `locals.user` to the `page` store for use inside client-side code
 export const load: LayoutServerLoad = async ({ fetch, depends }) => {
-	let card;
+	let balance;
 
 	try {
-		const response = await fetch(`${PUBLIC_REST_API_URL}/user/card`, {
+		const response = await fetch(`${PUBLIC_REST_API_URL}/user/balance`, {
 			method: 'POST'
 		});
 
@@ -14,30 +14,29 @@ export const load: LayoutServerLoad = async ({ fetch, depends }) => {
 
 		if (!response.ok) {
 			// When the response is not OK, the server will return an `errors` object
-			// In the case of no result, the server will return a 404 status code (that's the plan anyway)
+			// In the case of no card, the server will return a 404 status code (that's the plan anyway)
 			let message;
 			if (result.errors?.detail) message = result.errors.detail;
 			else if (result.errors?.message) message = result.errors.message;
 			else message = String(result.errors);
 
 			console.error(
-				`Server responded with an error when requesting card details: (${response.status}) ${message}`
+				`Server responded with an error when requesting balance: (${response.status}) ${message}`
 			);
 		}
-
-		card = result;
+		balance = result?.balance;
 	} catch (error) {
 		// These are unexpected errors, thrown when the server is unreachable, the response cannot be parsed, etc.
 		let message;
 		if (error instanceof Error) message = error.message;
 		else message = String(error);
 
-		console.error(`An unexpected error occurred when requesting card details: ${message}`);
+		console.error(`An unexpected error occurred when requesting balance: ${message}`);
 	}
 
-	depends('server:card');
+	depends('server:balance');
 
 	return {
-		card
+		balance
 	};
 };
